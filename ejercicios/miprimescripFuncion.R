c9507b093462b5b3219753d6f77d413a7dcb0c4c
ikoroso <65134278+ikoroso@users.noreply.github.com>
2020-05-26 23:45:39 +0200

    Create miprimescripFuncion.R

---
+++ ejercicios/miprimescripFuncion.R
@@ -0,0 +1,13 @@
+opBasic=function(a,b){
+  print ("Suma")
+  print (a+b)
+  print ("Resta")
+  print (a-b)
+  print ("Producto")
+  print (a*b)
+  print ("Cociente de la división entera")
+  print (a%/%b)
+  print ("Resto de la división entera")
+  print(a%%b)
+}
+opBasic(5,3)
